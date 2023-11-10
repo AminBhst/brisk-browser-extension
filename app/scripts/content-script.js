d1664f37fbb2b66a39cd6a874e12118bfe705fc4
AminBhst <amin.bhst@gmail.com>
2023-11-10 18:42:50 +0330

    Add support for downloading selected links

---
+++ app/scripts/content-script.js
@@ -0,0 +1,46 @@
+import * as browser from 'webextension-polyfill';
+
+function debounce(fn, delay) {
+    let timer = null;
+    return function () {
+        let context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(context, args);
+        }, delay);
+    };
+}
+
+document.addEventListener("selectionchange", debounce(function (event) {
+    let extractedHrefs = getHrefOfAllSelectedLinks();
+    sendHrefsToBackground(extractedHrefs);
+}, 250));
+
+
+function sendHrefsToBackground(hrefs) {
+    browser.runtime.sendMessage(hrefs);
+}
+
+const getHrefOfAllSelectedLinks = () => getSelectedNodes()
+    .filter(node => node.tagName === "A")
+    .map(node => node.href);
+
+
+function getSelectedNodes() {
+    const selection = document.getSelection();
+    const fragment = document.createDocumentFragment();
+    const nodeList = [];
+
+    for (let i = 0; i < selection.rangeCount; i++) {
+        fragment.append(selection.getRangeAt(i).cloneContents());
+    }
+
+    const walker = document.createTreeWalker(fragment);
+    let currentNode = walker.currentNode;
+    while (currentNode) {
+        nodeList.push(currentNode);
+        currentNode = walker.nextNode();
+    }
+
+    return nodeList;
+}
